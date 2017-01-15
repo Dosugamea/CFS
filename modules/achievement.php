d1d7b847d4faf246388465db61965218e68bda35
lijun0326 <lijun00326@gmail.com>
2017-01-15 11:49:02 +0800

    课题页修复（不炸就好了hhh）

--- modules/achievement.php
+++ modules/achievement.php
@@ -1,6 +1,12 @@
 <?php
 //课题界面 ！待修复！
 function achievement_unaccomplishList() {
-  return json_decode();
+	$ret = [];
+	$ret[] = json_decode('{
+			"achievement_category_id": 1,
+			"count": 0,
+			"achievement_list": []
+		}',true);
+	return $ret;
 }
 ?>
