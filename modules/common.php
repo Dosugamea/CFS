8d19a3139f707eaa9ea595d952925ff15b927899
zxl <zhaoxile2@hotmail.com>
2017-01-15 12:54:22 +0800

    真·修复LP恢复爆炸

--- modules/common.php
+++ modules/common.php
@@ -1,7 +1,8 @@
 <?php
 //common/recoveryEnergy 恢复LP防止崩溃
 function common_recoveryEnergy() {
-	$ret['error_code'] = "1102";
-	return $ret;
+	  return json_decode('{
+            "error_code": 1102,
+        }');
 }
 ?>
