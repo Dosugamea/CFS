c7603458827b0e9215ad80076c9f5fd376e2e6a1
NOAHLUALU <919400283@qq.com>
2017-02-15 23:39:40 +0800

    tos 页面

---
+++ webview/tos/read.php
@@ -0,0 +1,71 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="GENERATOR" content="MSHTML 11.00.10011.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=880, target-densitydpi=device-dpi, user-scalable=no">
+
+	<link rel="stylesheet" href="/resources/things/detail.css?">
+	<link rel="stylesheet" href="/resources/things/perfect-scrollbar.css">
+	<link rel="stylesheet" href="/resources/things/list2.css">
+
+	<script src="/resources/things/perfect-scrollbar.min.js"></script>
+	<script src="/resources/things/button.js"></script>
+	<style type="text/css">
+		.main-text-head{text-align: center;}
+		.time{width:100%;text-align: right;}
+	</style>
+
+</head>
+<body>
+	<div id="outer">
+  <div id="inner">
+    <div id="header">
+      <h2>使用协议</h2>
+
+    </div>
+
+<div id="body">
+
+      <div class="main-text-head">
+      	=========================================================<br>
+		<h1>Programmed Live!</h1><br>
+		Use Agreement <br>
+		使用协议<br>
+		=========================================================<br>
+
+		# If you continue to use the PL and affiliate items, <br>
+		you have agreed to all of the following agreements, <br>
+		please suspend your use if you have any concerns, <br>
+		thank you for your cooperation！<br><br>
+
+		#如果您继续使用PL已及附属项目，<br>
+		表示您已同意以下所有协议，<br>
+		如有异议请暂停使用，谢谢合作！<br>
+		------------------------------------------------------------------------------------------------------
+	 </div><br>
+	 <div>
+	 	#1.本服务端以及所属的代码以及修改版的客户端用途为研究学习交流使用为个人开发,严禁在公开场合传播宣传并且禁止用于商业用途,一经发现我们将会采取相应措施<br /><br />
+		#2.严禁在公开场合（贴吧、微博等）发布与PL有关的任何消息、截图、视频等
+		（特别的，在公开场合公开了群号的QQ群属于公开场合）<br /><br />
+		#3.若私下传播，则必须确保【所有看到消息的人也遵守前面一条】<br /><br />
+	 </div><br /><br />
+	 <div class="time">
+	 	Programmed Live!<br>
+	 	2017/02/15 23:30:00
+	 </div>
+      <br><br><br>
+
+
+
+ </div>
+  </div>
+</div>
+</div>
+  </div>
+</div>
+
+<script>
+  Ps.initialize(document.getElementById('body'), {suppressScrollX: true});
+</script>
+</body>
+</body>
