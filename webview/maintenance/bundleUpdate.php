8a9ed862ad225eb433c0e0f87a10873d13939cc5
lijun0326 <lijun00326@gmail.com>
2017-03-12 19:53:12 +0800

    更新【客户端更新】页

--- webview/maintenance/bundleUpdate.php
+++ webview/maintenance/bundleUpdate.php
@@ -2,48 +2,49 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>メンテナンス</title>
-<link rel="stylesheet" href="/resources/bstyle.css">
-<script type="text/javascript">
-var strUA = "";
-strUA = navigator.userAgent.toLowerCase();
+	<meta charset="utf-8">
+	<meta name="GENERATOR" content="MSHTML 11.00.10011.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=880, target-densitydpi=device-dpi, user-scalable=no">
 
-if(strUA.indexOf("iphone") >= 0) {
-  document.write('<meta name="viewport" content="width=960px, minimum-scale=0.45, maximum-scale=0.45, user-scalable=no" />');
-} else if (strUA.indexOf("ipad") >= 0) {
-  document.write('<meta name="viewport" content="width=1024px, minimum-scale=0.9, maximum-scale=0.9, user-scalable=no" />');
-} else if (strUA.indexOf("android 2.3") >= 0) {
-  document.write('<meta name="viewport" content="width=960px, minimum-scale=0.45, maximum-scale=0.45, initial-scale=0.45, user-scalable=yes" />');
-} else {
-  document.write('<meta name="viewport" content="width=960px, minimum-scale=0.38, maximum-scale=0.38, user-scalable=no" />');
-}
-</script>
+	<link rel="stylesheet" href="/resources/things/detail.css?">
+	<link rel="stylesheet" href="/resources/things/perfect-scrollbar.css">
+	<link rel="stylesheet" href="/resources/things/list2.css">
+
+	<script src="/resources/things/perfect-scrollbar.min.js"></script>
+	<script src="/resources/things/button.js"></script>
+	<style type="text/css">
+		.main-text-head{text-align: center;}
+		.time{width:100%;text-align: right;}
+	</style>
 </head>
-
 <body>
-<div id="wrapper">
-<div class="title">
-<img src="/resources/bg01_update.png" width="95%">
-</div>
+	<div id="outer">
+  <div id="inner">
+    <div id="header">
+      <h2>版本升级</h2>
 
-<div class="content">
-  <div class="note">
-<p>
+    </div>
+
+<div id="body">
+
 请升级您的客户端版本。<br />
 <br />
 
 服务器版本：<?=$bundle_ver ?><br />
 <br />
 客户端版本：<?=$_SESSION['server']['HTTP_BUNDLE_VERSION'] ?>（过低）<br />
-<br />
-<br />
-</p>
+
+
+</div>
   </div>
 </div>
-<div class="footer">
-<img src="/resources/bg03.png" width="95%">
 </div>
+  </div>
 </div>
+
+<script>
+  Ps.initialize(document.getElementById('body'), {suppressScrollX: true});
+</script>
 </body>
 </html>
