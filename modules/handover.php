8c6211e7eafdbb995ea4a2a9454cf5342d1fdad2
lijun0326 <lijun00326@gmail.com>
2017-03-14 21:30:29 +0800

    新版继承码防崩

--- modules/handover.php
+++ modules/handover.php
@@ -1,6 +1,7 @@
 <?php
 //账户界面发行继承码 返回空值
 function handover_start() {
+	$ret['id'] = "UR1234567";
 	$ret['code'] = "0000000000000000";
 	$ret['expire_date'] = "不支持發行繼承碼！";
 	return $ret;
@@ -8,6 +9,7 @@ function handover_start() {
 
 //更新防止崩溃 返回空
 function handover_renew() {
+	$ret['id'] = "UR1234567";
 	$ret['code'] = "0000000000000000";
 	$ret['expire_date'] = "不支持發行繼承碼！";
 	return $ret;
