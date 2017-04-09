01ed1a0c5bf32fffb6255030e5d8162b45f1a678
lijun0326 <lijun00326@gmail.com>
2017-04-09 13:20:22 +0800

    强迫症+1

--- modules/api.php
+++ modules/api.php
@@ -2,19 +2,19 @@
 //api.php 批量执行指令
 
 function api_($post) {
-  foreach($post as $v) {
-    $ret2['status']=200;
-    $ret2['commandNum']=false;
-    $ret2['timestamp']=time();
-    $module=$v['module'];
-    $action=$v['action'];
-    unset($v['module']);
-    unset($v['action']);
-    unset($v['timeStamp']);
-    $ret2['result']=runAction($module, $action, $v);
-    $ret[]=$ret2;
-  }
-  return $ret;
+	foreach($post as $v) {
+		$module=$v['module'];
+		$action=$v['action'];
+		$ret2['result']=runAction($module, $action, $v);
+		$ret2['status']=200;
+		$ret2['commandNum']=false;
+		$ret2['timestamp']=time();
+		unset($v['module']);
+		unset($v['action']);
+		unset($v['timeStamp']);
+		$ret[]=$ret2;
+	}
+	return $ret;
 }
 
 ?>
