3d76efcd2a3ec9049b6656681a6624b9752c80c8
lijun0326 <lijun00326@gmail.com>
2017-06-02 13:09:53 +0800

    继续修LP

--- includes/energy.php
+++ includes/energy.php
@@ -11,7 +11,7 @@ function getCurrentEnergy(){
 		$ret['energy_full_time'] = $energy['energy_full_time'];
 		$ret['over_max_energy'] = (int)$energy['over_max_energy'];
 	}else{
-		$ret['energy_full_need_time'] = strtotime($energy['energy_full_time']) - strtotime("now");
+		$ret['energy_full_need_time'] = (strtotime($energy['energy_full_time']) - strtotime("now")) < 0 ? 0 : (strtotime($energy['energy_full_time']) - strtotime("now"));
 		$ret['energy_full_time'] = $energy['energy_full_time'];
 		$ret['over_max_energy'] = 0;
 	}
@@ -22,7 +22,10 @@ function energyDecrease($amount){
 	global $uid, $mysql;
 	$energy = $mysql->query("SELECT energy_full_time, over_max_energy, level FROM users WHERE user_id = ".$uid)->fetch(PDO::FETCH_ASSOC);
 	$energy_max = 100 + floor($energy['level']/2);
-	$energy_now = $energy_max - floor((strtotime($energy['energy_full_time']) - strtotime("now"))/360);
+	if((strtotime($energy['energy_full_time']) - strtotime("now")) < 0)
+		$energy_now = $energy_max;
+	else
+		$energy_now = $energy_max - floor((strtotime($energy['energy_full_time']) - strtotime("now"))/360);
 	if($energy['over_max_energy'] > $energy_max){
 		if(($energy['over_max_energy'] - $amount) > $energy_max){
 			$mysql->query("UPDATE users SET over_max_energy = ".($energy['over_max_energy'] - $amount)." WHERE user_id = ".$uid);
@@ -34,7 +37,10 @@ function energyDecrease($amount){
 			return true;
 		}
 	}else if(($energy_now - $amount) >= 0){
-		$mysql->query("UPDATE users SET energy_full_time = '". date("Y-m-d H:i:s",(strtotime($energy['energy_full_time']) + $amount * 360))."' WHERE user_id = ".$uid);
+		if($energy_now == $energy_max)
+			$mysql->query("UPDATE users SET energy_full_time = '". date("Y-m-d H:i:s",(time() + $amount * 360))."' WHERE user_id = ".$uid);
+		else
+			$mysql->query("UPDATE users SET energy_full_time = '". date("Y-m-d H:i:s",(strtotime($energy['energy_full_time']) + $amount * 360))."' WHERE user_id = ".$uid);
 		return true;
 	}else
 		return false;
