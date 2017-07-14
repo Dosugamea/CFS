4faa9207519e63ddb9a55bab6ba7a21c82300ce1
lijun0326 <lijun00326@gmail.com>
2017-07-14 20:27:42 +0800

    改了下缩进

--- includes/live.php
+++ includes/live.php
@@ -1,128 +1,128 @@
 <?php 
 function getLiveSettingsFromCustomLive($id, $rows) {
-  global $mysql;
-  $result = $mysql->query('select * from programmed_live where ID=?', [$id])->fetch();
-  if ($result) {
-    $settings = json_decode($result['live_json'], true);
-    if (isset($settings['pl_auto_calculate_combo_rank'])) {
-      $map = $mysql->query('SELECT notes_list FROM notes_setting WHERE notes_setting_asset=?', [$result['notes_setting_asset']])->fetchColumn();
-      if ($map) {
-        $map = json_decode($map, true);
-        if (is_array($map)) {
-          $combo = count($map);
-          $settings['c_rank_combo'] = ceil($combo * 0.3);
-          $settings['b_rank_combo'] = ceil($combo * 0.5);
-          $settings['a_rank_combo'] = ceil($combo * 0.7);
-          $settings['s_rank_combo'] = $combo;
-          unset($settings['pl_auto_calculate_combo_rank']);
-          $mysql->query('update programmed_live set live_json=? where ID=?', [json_encode($settings), $id]);
-        }
-      }
-    }
-    
-    $settings['notes_setting_asset'] = $result['notes_setting_asset'];
-    $settings['capital_type'] = 1;
-    if (!isset($settings['difficulty'])) {
-      if (strtolower(substr($settings['name'], 0, 6)) == '[easy]' || $settings['stage_level'] <= 4) {
-        $settings['difficulty'] = 1;
-        $settings['capital_value'] = 5;
-      } else if (strtolower(substr($settings['name'], 0, 8)) == '[normal]' || $settings['stage_level'] <= 6) {
-        $settings['difficulty'] = 2;
-        $settings['capital_value'] = 10;
-      } else if (strtolower(substr($settings['name'], 0, 6)) == '[hard]' || $settings['stage_level'] <= 8) {
-        $settings['difficulty'] = 3;
-        $settings['capital_value'] = 15;
-      } else {
-        $settings['difficulty'] = 4;
-        $settings['capital_value'] = 25;
-      }
-    }
-    $ret = [];
-    $cnt = 0;
-    $settings['member_category'] = 2;
-    foreach (explode(',', $rows) as $v) {
-      $v = trim($v);
-      $ret[$v] = $settings[$v];
-      $ret[$cnt++] = $settings[$v];
-    }
-    return $ret;
-  }
-  trigger_error('getLiveSettingsFromCustomLive: 找不到live'.$id);
+	global $mysql;
+	$result = $mysql->query('select * from programmed_live where ID=?', [$id])->fetch();
+	if ($result) {
+		$settings = json_decode($result['live_json'], true);
+		if (isset($settings['pl_auto_calculate_combo_rank'])) {
+			$map = $mysql->query('SELECT notes_list FROM notes_setting WHERE notes_setting_asset=?', [$result['notes_setting_asset']])->fetchColumn();
+			if ($map) {
+				$map = json_decode($map, true);
+				if (is_array($map)) {
+					$combo = count($map);
+					$settings['c_rank_combo'] = ceil($combo * 0.3);
+					$settings['b_rank_combo'] = ceil($combo * 0.5);
+					$settings['a_rank_combo'] = ceil($combo * 0.7);
+					$settings['s_rank_combo'] = $combo;
+					unset($settings['pl_auto_calculate_combo_rank']);
+					$mysql->query('update programmed_live set live_json=? where ID=?', [json_encode($settings), $id]);
+				}
+			}
+		}
+		
+		$settings['notes_setting_asset'] = $result['notes_setting_asset'];
+		$settings['capital_type'] = 1;
+		if (!isset($settings['difficulty'])) {
+			if (strtolower(substr($settings['name'], 0, 6)) == '[easy]' || $settings['stage_level'] <= 4) {
+				$settings['difficulty'] = 1;
+				$settings['capital_value'] = 5;
+			} else if (strtolower(substr($settings['name'], 0, 8)) == '[normal]' || $settings['stage_level'] <= 6) {
+				$settings['difficulty'] = 2;
+				$settings['capital_value'] = 10;
+			} else if (strtolower(substr($settings['name'], 0, 6)) == '[hard]' || $settings['stage_level'] <= 8) {
+				$settings['difficulty'] = 3;
+				$settings['capital_value'] = 15;
+			} else {
+				$settings['difficulty'] = 4;
+				$settings['capital_value'] = 25;
+			}
+		}
+		$ret = [];
+		$cnt = 0;
+		$settings['member_category'] = 2;
+		foreach (explode(',', $rows) as $v) {
+			$v = trim($v);
+			$ret[$v] = $settings[$v];
+			$ret[$cnt++] = $settings[$v];
+		}
+		return $ret;
+	}
+	trigger_error('getLiveSettingsFromCustomLive: 找不到live'.$id);
 }
 
 //getLiveSettings 根据live_difficulty_id获取live_settings_m，外部访问无法调用
 function getLiveSettings($id, $rows) {
-  if ($id < 0) {
-    return getLiveSettingsFromCustomLive(0 - $id, $rows);
-  }
-  global $mysql;
-  $live = getLiveDb();
-  foreach (['normal_live_m', 'special_live_m', 'marathon.event_marathon_live_m', 'battle.event_battle_live_m', 'festival.event_festival_live_m'] as $v) {
-    $ret = $live->query ('
-      SELECT '.$rows.' FROM live_setting_m
-      LEFT JOIN '.$v.' ON '.$v.'.live_setting_id = live_setting_m.live_setting_id
-      LEFT JOIN live_track_m ON live_track_m.live_track_id = live_setting_m.live_track_id
-      WHERE live_difficulty_id = '.$id
-    )->fetch();
-    if($ret) {
-      return $ret;
-    }
-  }
-  trigger_error('getLiveSettings: 找不到live'.$id);
+	if ($id < 0) {
+		return getLiveSettingsFromCustomLive(0 - $id, $rows);
+	}
+	global $mysql;
+	$live = getLiveDb();
+	foreach (['normal_live_m', 'special_live_m', 'marathon.event_marathon_live_m', 'battle.event_battle_live_m', 'festival.event_festival_live_m'] as $v) {
+		$ret = $live->query ('
+			SELECT '.$rows.' FROM live_setting_m
+			LEFT JOIN '.$v.' ON '.$v.'.live_setting_id = live_setting_m.live_setting_id
+			LEFT JOIN live_track_m ON live_track_m.live_track_id = live_setting_m.live_track_id
+			WHERE live_difficulty_id = '.$id
+		)->fetch();
+		if($ret) {
+			return $ret;
+		}
+	}
+	trigger_error('getLiveSettings: 找不到live'.$id);
 }
 
 //getRankInfo 获取rank_info，外部访问无法调用
 function getRankInfo($id) {
-  if ($id < 0) {
-    $rank = getLiveSettingsFromCustomLive(0 - $id, 'c_rank_score,b_rank_score,a_rank_score,s_rank_score');
-    array_unshift($rank, 0);
-    array_push($rank, 1);
-  } else {
-    $rank = getLiveSettings($id, '0 as a, live_setting_m.c_rank_score, live_setting_m.b_rank_score, live_setting_m.a_rank_score, live_setting_m.s_rank_score');
-  }
-  $ret = [];
-  for ($i = 5; $i > 0; $i--) {
-    $r['rank'] = $i;
-    $r['rank_min'] = (int)$rank[5 - $i];
-    $ret[] = $r;
-  }
-  return $ret;
+	if ($id < 0) {
+		$rank = getLiveSettingsFromCustomLive(0 - $id, 'c_rank_score,b_rank_score,a_rank_score,s_rank_score');
+		array_unshift($rank, 0);
+		array_push($rank, 1);
+	} else {
+		$rank = getLiveSettings($id, '0 as a, live_setting_m.c_rank_score, live_setting_m.b_rank_score, live_setting_m.a_rank_score, live_setting_m.s_rank_score');
+	}
+	$ret = [];
+	for ($i = 5; $i > 0; $i--) {
+		$r['rank'] = $i;
+		$r['rank_min'] = (int)$rank[5 - $i];
+		$ret[] = $r;
+	}
+	return $ret;
 }
 
 function detectSameTiming(&$notes) {
-  $sameTimingMap = [];
-  foreach($notes as $e) {
-    if (isset($sameTimingMap[$e['timing_sec']])) {
-      ++$sameTimingMap[$e['timing_sec']];
-    } else {
-      $sameTimingMap[$e['timing_sec']] = 1;
-    }
-  }
-  foreach($notes as &$v) {
-    if ($sameTimingMap[$v['timing_sec']] > 1) {
-      $v['is_same_timing'] = true;
-    }
-  }
+	$sameTimingMap = [];
+	foreach($notes as $e) {
+		if (isset($sameTimingMap[$e['timing_sec']])) {
+			++$sameTimingMap[$e['timing_sec']];
+		} else {
+			$sameTimingMap[$e['timing_sec']] = 1;
+		}
+	}
+	foreach($notes as &$v) {
+		if ($sameTimingMap[$v['timing_sec']] > 1) {
+			$v['is_same_timing'] = true;
+		}
+	}
 }
 
 //对谱面按时间排序以免随机谱爆炸
 function mapSort($arr)
-{  
-  $len=count($arr);
-  //该层循环控制 需要冒泡的轮数
-  for($i=1;$i<$len;$i++)
-  { //该层循环用来控制每轮 冒出一个数 需要比较的次数
-    for($k=0;$k<$len-$i;$k++)
-    {
-       if($arr[$k]['timing_sec']>$arr[$k+1]['timing_sec'])
-        {
-            $tmp=$arr[$k+1];
-            $arr[$k+1]=$arr[$k];
-            $arr[$k]=$tmp;
-        }
-    }
-  }
-  return $arr;
+{	
+	$len=count($arr);
+	//该层循环控制 需要冒泡的轮数
+	for($i=1;$i<$len;$i++)
+	{ //该层循环用来控制每轮 冒出一个数 需要比较的次数
+		for($k=0;$k<$len-$i;$k++)
+		{
+			 if($arr[$k]['timing_sec']>$arr[$k+1]['timing_sec'])
+				{
+						$tmp=$arr[$k+1];
+						$arr[$k+1]=$arr[$k];
+						$arr[$k]=$tmp;
+				}
+		}
+	}
+	return $arr;
 }
 
 function generateRandomLive($note) {
@@ -295,20 +295,20 @@ function generateRandomLiveLimitless($note) {
 	foreach($note as $v)
 		$timing[]=$v['timing_sec'];
 	array_multisort($timing,SORT_ASC,$note);
-    
-  $holding=[0,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1];
-    
+		
+	$holding=[0,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1];
+		
 	foreach($note as $k=>&$v){
-    while(true){
-      $v['position']=rand(1,9);
-      if($v['timing_sec']>$holding[$v['position']]+0.05)
-        break;
-    }
-    if($v['effect']%10==3) //长条
+		while(true){
+			$v['position']=rand(1,9);
+			if($v['timing_sec']>$holding[$v['position']]+0.05)
+				break;
+		}
+		if($v['effect']%10==3) //长条
 			$holding[$v['position']]=$v['timing_sec']+$v['effect_value'];
-    else
-      $holding[$v['position']]=$v['timing_sec'];
-  }
+		else
+			$holding[$v['position']]=$v['timing_sec'];
+	}
 	return $note;
 }
 
@@ -319,37 +319,36 @@ function beatmap_timing_cmp($u, $v) {
 
 //calcScore 计算分数
 function calcScore($base, $map) {
-  $total = 0;
-  $combo = 0;
-  $rate = 1;
+	$total = 0;
+	$combo = 0;
+	$rate = 1;
 
 
-  if (isset($map[0]['timing_sec'])) {
-    $map_ = [['live_info'=>Null]];
+	if (isset($map[0]['timing_sec'])) {
+		$map_ = [['live_info'=>Null]];
 	$map_[0]['live_info']['notes_list'] = $map;
 	$map = $map_;
-  }
-  foreach($map as $v2) {
+	}
+	foreach($map as $v2) {
+		foreach($v2['live_info']['notes_list'] as $k => &$p) $p['timing_sec'] += ($p['effect'] % 10 == 3) * ($p['effect_value']);
+		usort($v2['live_info']['notes_list'], 'beatmap_timing_cmp');
 
-	  foreach($v2['live_info']['notes_list'] as $k => &$p) $p['timing_sec'] += ($p['effect'] % 10 == 3) * ($p['effect_value']);
-	  usort($v2['live_info']['notes_list'], 'beatmap_timing_cmp');
 
-
-	  $total += array_reduce($v2['live_info']['notes_list'], function ($sum, $next) use (&$combo, $base, &$rate) {
-      $combo++;
-      switch($combo) {
-      case 51:$rate = 1.1;break;
-      case 101:$rate = 1.15;break;
-      case 201:$rate = 1.2;break;
-      case 401:$rate = 1.25;break;
-      case 601:$rate = 1.3;break;
-      case 801:$rate = 1.35;break;
-      }
-      $score = $base * 1.25 * $rate;
-      if ($next['effect'] > 10) $score *= 0.5;
-      if ($next['effect'] % 10 == 3) $score *= 1.25;
-      return $sum + floor($score / 100);
-    }, 0);
-  }
-  return $total;
+		$total += array_reduce($v2['live_info']['notes_list'], function ($sum, $next) use (&$combo, $base, &$rate) {
+			$combo++;
+			switch($combo) {
+			case 51:$rate = 1.1;break;
+			case 101:$rate = 1.15;break;
+			case 201:$rate = 1.2;break;
+			case 401:$rate = 1.25;break;
+			case 601:$rate = 1.3;break;
+			case 801:$rate = 1.35;break;
+			}
+			$score = $base * 1.25 * $rate;
+			if ($next['effect'] > 10) $score *= 0.5;
+			if ($next['effect'] % 10 == 3) $score *= 1.25;
+			return $sum + floor($score / 100);
+		}, 0);
+	}
+	return $total;
 }
