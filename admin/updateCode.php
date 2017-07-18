fa663902ecf2a720fd1aee4cdf084ed17e294056
xfl03 <xfl03@hotmail.com>
2017-07-19 01:27:05 +0800

    快速部署master

---
+++ admin/updateCode.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+include_once("includes/check_admin.php");
+exec('cd .. & git pull origin master', $output, $return_val); 
+print_r($output);
+?>
+</pre>
+<a href='javascript:history.go(-1);'>返回上一页</a>
