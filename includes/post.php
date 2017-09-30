36e0bf751926f82b0f8b55a84d4e87374ecc46fe
lijun0326 <lijun00326@gmail.com>
2017-09-30 22:31:29 +0800

    post对象化，使用token缓存机制

--- includes/post.php
+++ includes/post.php
@@ -1,47 +1,13 @@
 <?php
-function curls($url, $headers, $data_string) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_HEADER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    $data = curl_exec($ch);
-    curl_close($ch);
-	$resp = explode("\r\n\r\n", $data);
-	$header = explode("\r\n", $resp[0]);
-	$resp[1] = json_decode($resp[1],true);
-	$resp[0] = $header;
-	//$resp[1] = $body;
-    return $resp;
-}
-
-function login(){
-	$time = time();
-	include 'config/maintenance.php';
-	//生成随机AES密钥
-	$chars='ABDEFGHJKLMNPQRSTVWXYabdefghijkmnpqrstvwxy23456789#%*';
-	mt_srand((double)microtime()*1000000*getmypid());
-	$AES_token_client='';
-	while(strlen($AES_token_client) < 32){
-		$AES_token_client.=substr($chars,(mt_rand()%strlen($chars)),1);  
-	}
-	//生成IV
-	$iv='';
-	while(strlen($iv) < 16){
-		$iv.=substr($chars,(mt_rand()%strlen($chars)),1);  
-	}
-	//计算HMAC密钥
-	$xor_table = hex2bin("02081105200d161d0d040c0a5b0509050f440b100651150159620a0e02581c0e");
-	for($i=0;$i < strlen($xor_table);$i++){
-		$HMACKey[$i] = ($xor_table[$i] ^ $AES_token_client[$i % strlen($AES_token_client)]);
-	}
-	$HMACKey = implode("",$HMACKey);
-	//RSA加密密钥
-	$encrypted_AES_token_client = RSAencrypt($AES_token_client);
-	//auth_data生成
-	$device_data = '{
+class poster{
+	private $login_key;
+	private $login_passwd;
+	private $application_key = "gae1aefu9eeRojahj4cei3pho1eaZada";
+	private $xor_base = "eit4Ahph4aiX4ohmephuobei6SooX9xo";
+	private $cache;
+	private $sessionKey = "";
+	private $uri = "http://prod-jp.lovelive.ge.klabgames.net/main.php";
+	private $device_data = '{
 		"Hardware":"Qualcomm Technologies, Inc MSM8974",
 		"adbEnbled":"NO",
 		"basePath":"/data/user/0/klb.android.lovelive/files",
@@ -54,20 +20,16 @@ function login(){
 		"ro.product.manufacturer":"LG",
 		"ro.product.model":"Nexus 5",
 		"ro.product.name":"hammerhead",
-		"signature":"ba5803d889a34bd5a974b7d43d688799c645a70f4aa86628198ccfe0e1df804dcc5b4a158920603a6f5e2a709c494aa49ae09e198c56b4a116bf7a1f6c21c2e8",
+		"signature":"b3dc6cc00b7dec39fcdf40a5ea1c986f2b3c0ce05c50120cc3f5d17e47916bcd730bbdead2c5fbaf14fc0b3e199f48f9a8483164776226310b8089aeb0bbdbca",
 		"SuspiciousElement":[]
 	}';
-	include 'config/modules_download.php';
-	$auth_data = json_encode(["1"=>$login_key,"2"=>$login_passwd,"3"=>base64_encode($device_data)]);
-	$auth_data_enc = AESencrypt($auth_data, substr($AES_token_client,0,16), $iv);
-	//生成body
-	$body = array(
-		"request_data" => json_encode(array(
-			"dummy_token" => $encrypted_AES_token_client,
-			"auth_data" => base64_encode($iv.base64_decode($auth_data_enc))
-	)));
+	private $common_headers;
 	
-	$headers = array(
+	public function __construct(){
+		global $official_bundle_ver, $server_ver;
+		include("config/modules_download.php");
+		$time = time();
+		$this->common_headers = [
 			'Accept: */*',
 			'Accept-Encoding: deflate',
 			'API-Model: straightforward',
@@ -80,43 +42,138 @@ function login(){
 			'Application-ID: 626776655',
 			'Time-Zone: JST',
 			'Reigion: 392',
-			'Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&nonce=1',
-			'Expect:'
-		);
-	$XMC = hash_hmac('sha1', $body['request_data'], $HMACKey);
-	$headers[] = "X-Message-Code: $XMC";
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/login/authkey",$headers,$body);
-	if(in_array("Maintenance: 1",$r[0])){
-		return "Maintenance";
+			"Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&nonce=1",
+			'Expect:',
+			'X-Message-Code: '
+		];
+		if(is_file("auth.cache") && filesize("auth.cache") > 0){
+			$cache_file = fopen("auth.cache", "r+");
+			$cache_file_ = fread($cache_file, filesize("auth.cache"));
+		}else{
+			$cache_file = fopen("auth.cache", "w");
+		}
+		
+		if(!isset($cache_file_) || !$cache_file_){
+			$this->cache = ["token" => "", "time" => 0, "sessionKey" => "", "uid" => 0];
+		}else{
+			$this->cache = json_decode($cache_file_, true);
+			$this->common_headers[12] = "Authorize: consumerKey=lovelive_test&timeStamp=".time()."&version=1.1&token=".$this->cache['token']."&nonce=2";
+		}
+		$sessionKey = $this->cache['sessionKey'];
+		$this->sessionKey = base64_decode($sessionKey);
+		$this->login_key = $login_key;
+		$this->login_passwd = $login_passwd;
+		$this->common_headers[] = "User-ID: ".$this->cache['uid'];
+		if(time() - $this->cache['time'] > 86400){ //每24小时刷新token
+			$this->login();
+			$this->cache['sessionKey'] = base64_encode($this->sessionKey);
+			rewind($cache_file);
+			fwrite($cache_file, json_encode($this->cache));
+		}
+		fclose($cache_file);
 	}
-	$r = $r[1];
-	
-	$token = $r['response_data']['authorize_token'];
-	$AES_token_server = base64_decode($r['response_data']['dummy_token']);
-	//计算新的密钥
-	for($i=0;$i < strlen($AES_token_server);$i++){
-		$sessionKey[$i] = ($AES_token_server[$i] ^ $AES_token_client[$i % strlen($AES_token_client)]);
+	private function curls($url, $headers, $data_string) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		$resp = explode("\r\n\r\n", $data);
+		$header = explode("\r\n", $resp[0]);
+		$resp[1] = json_decode($resp[1],true);
+		$resp[0] = $header;
+		return $resp;
 	}
-	$sessionKey = implode("",$sessionKey);
 	
-	$time = time();
-	$headers[12] = "Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=2";
+	public function post($body, $url){
+		$full_url = $this->uri.$url;
+		$req_body = ["request_data" => json_encode($body)];//这里的body一定要是"request_data" => json的形式！
+		$headers = $this->common_headers;
+		$XMC = hash_hmac('sha1', $req_body['request_data'], $this->sessionKey);
+		$headers[14] = "X-Message-Code: $XMC";
+		return $this->curls($full_url, $headers, $req_body);
+	}
 	
-	
-	//这里的body一定要是"request_data" => json的形式！
-	$body = array(
-		"request_data" => json_encode(array(
-			"login_key" => base64_encode($iv.base64_decode(AESencrypt($login_key,substr($sessionKey,0,16),$iv))),
-			"login_passwd" => base64_encode($iv.base64_decode(AESencrypt($login_passwd,substr($sessionKey,0,16),$iv))),
-			"devtoken" => "APA91bFb7105jsH-vKhCv1XCxJaj3e2g9sEpSCLWtZvWniRhpv-DvHI_pvj7jYGwCGPbVejKfDwXsHXzez3loz95HJoam8xpQnfo-pCHZEAhiBW9IWzfV5-__k3BS5_lRx9zLqJ-LGKq"
-	)));
-	$XMC = hash_hmac('sha1', $body['request_data'], $sessionKey);
-	$headers[14] = "X-Message-Code: $XMC";
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/login/login",$headers,$body);
-	//$r = json_decode(gzdecode($r));
-	$r = $r[1]['response_data'];
-	$r['sessionKey'] = $sessionKey;
-	return $r;
+	private function login(){
+		unset($this->common_headers[15]);
+		$time = time();
+		include 'config/maintenance.php';
+		//生成随机AES密钥
+		$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890/*-+!@#$%^&*()_=';
+		mt_srand((double)microtime() * 1000000 * getmypid());
+		$AES_token_client = '';
+		while(strlen($AES_token_client) < 32){
+			$AES_token_client .= substr($chars,(mt_rand()%strlen($chars)),1);  
+		}
+		//生成IV
+		$iv = '';
+		while(strlen($iv) < 16){
+			$iv .= substr($chars, (mt_rand() % strlen($chars)), 1);  
+		}
+		//计算HMAC密钥
+		for($i = 0;$i < 32;$i++){
+			$xor_table[$i] = $this->application_key[$i] ^ $this->xor_base[$i];
+		}
+		$xor_table = implode("", $xor_table);
+		for($i = 0;$i < strlen($xor_table);$i++){
+			$HMACKey[$i] = $xor_table[$i] ^ $AES_token_client[$i % strlen($AES_token_client)];
+		}
+		$this->sessionKey = implode("",$HMACKey);
+		//RSA加密密钥
+		$encrypted_AES_token_client = RSAencrypt($AES_token_client);
+		//auth_data生成
+		$auth_data = json_encode(["1"=>$this->login_key,"2"=>$this->login_passwd,"3"=>base64_encode($this->device_data)]);
+		$auth_data_enc = AESencrypt($auth_data, substr($AES_token_client,0,16), $iv);
+		//生成body
+		$body = [
+			"dummy_token" => $encrypted_AES_token_client,
+			"auth_data" => base64_encode($iv.base64_decode($auth_data_enc))
+		];
+		$r = $this->post($body, "/login/authkey");
+		if(in_array("Maintenance: 1",$r[0])){
+			return "Maintenance";
+		}
+		$r = $r[1];
+		
+		$token = $r['response_data']['authorize_token'];
+		$AES_token_server = base64_decode($r['response_data']['dummy_token']);
+		//计算新的密钥
+		for($i = 0;$i < strlen($AES_token_server);$i++){
+			$sessionKey[$i] = ($AES_token_server[$i] ^ $AES_token_client[$i % strlen($AES_token_client)]);
+		}
+		$sessionKey = implode("",$sessionKey);
+		$this->sessionKey = $sessionKey;
+		
+		$time = time();
+		$this->common_headers[12] = "Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=2";
+		
+		$devtoken = "";
+		for($i = 0; $i < 64; $i++){
+			$devtoken .= str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
+			$devtoken .= ":";
+		}
+		$devtoken = substr($devtoken, 0, -1);
+		$body = [
+				"login_key" => base64_encode($iv.base64_decode(AESencrypt($this->login_key, substr($sessionKey,0,16),$iv))),
+				"login_passwd" => base64_encode($iv.base64_decode(AESencrypt($this->login_passwd, substr($sessionKey,0,16),$iv))),
+				"devtoken" => strtoupper($devtoken)
+		];
+		$r = $this->post($body, "/login/login");
+		$r = $r[1]['response_data'];
+		@$this->cache['token'] = $r['authorize_token'];
+		@$token = $r['authorize_token'];
+		@$user_id = $r['user_id'];
+		$time = time();
+		$this->common_headers[12] = "Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=3";
+		
+		@$this->common_headers[] = "User-ID: $user_id";
+		@$this->cache['uid'] = $user_id;
+		$this->cache['time'] = time();
+		return;
+	}
 }
-
 ?>
--- modules/download.php
+++ modules/download.php
@@ -15,46 +15,12 @@ function download_additional($post) {
 	return $ret;
 	*/
 	include 'config/maintenance.php';
-	$tokenanduid = login();
-	$token = $tokenanduid['authorize_token'];
-	$user_id = $tokenanduid['user_id'];
-	$sessionKey = $tokenanduid['sessionKey'];
-	$os = $post['os'];
-	if($post['os'] == 'Android'){
-		$PlatformType = 2;
-	}else{
-		$PlatformType = 1;
-	}
-	
-	$time = time();
-	$headers = array(
-			'Accept: */*',
-			'Accept-Encoding: deflate',
-			'API-Model: straightforward',
-			'Debug: 1',
-			"Bundle-Version: $official_bundle_ver",
-			"Client-Version: $official_client_ver",
-			'OS-Version: Nexus 5 google hammerhead 4.4.4',
-			"OS: $os",
-			"Platform-Type: $PlatformType",
-			'Application-ID: 626776655',
-			'Time-Zone: JST',
-			'Reigion: 392',
-			"Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=3",
-			'Expect:'
-		);
 	
 	$post['timeStamp'] = time();
 	$post['commandNum'] = $login_key.".".time()."."."3";
 	
-	$XMC = hash_hmac('sha1', json_encode($post), $sessionKey);
-	$headers[] = "X-Message-Code: $XMC";
-	$headers[] = "User-ID: $user_id";
-	
-	$post_server = array(
-		"request_data" => json_encode($post)
-	);
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/download/additional",$headers,$post_server);
+	$poster = new poster();
+	$r = $poster->post($post, "/download/additional");
 	$ret = $r[1]['response_data'];
 	$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
 	if($download_site == "1"){
@@ -79,49 +45,12 @@ function download_batch($post) {
 	}));
 	return $ret;*/
 	include 'config/maintenance.php';
-	$tokenanduid = login();
-	if($tokenanduid == "Maintenance"){
-		return [];
-	}
-	$token = $tokenanduid['authorize_token'];
-	$user_id = $tokenanduid['user_id'];
-	$sessionKey = $tokenanduid['sessionKey'];
-	$os = $post['os'];
-	if($post['os'] == 'Android'){
-		$PlatformType = 2;
-	}else{
-		$PlatformType = 1;
-	}
-	
-	$time = time();
-	$headers = array(
-			'Accept: */*',
-			'Accept-Encoding: deflate',
-			'API-Model: straightforward',
-			'Debug: 1',
-			"Bundle-Version: $official_bundle_ver",
-			"Client-Version: $official_client_ver",
-			'OS-Version: Nexus 5 google hammerhead 4.4.4',
-			"OS: $os",
-			"Platform-Type: $PlatformType",
-			'Application-ID: 626776655',
-			'Time-Zone: JST',
-			'Reigion: 392',
-			"Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=3",
-			'Expect:'
-		);
 	
 	$post['timeStamp'] = time();
 	$post['commandNum'] = $login_key.".".time()."."."3";
 	
-	$XMC = hash_hmac('sha1', json_encode($post), $sessionKey);
-	$headers[] = "X-Message-Code: $XMC";
-	$headers[] = "User-ID: $user_id";
-	
-	$post_server = array(
-		"request_data" => json_encode($post)
-	);
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/download/batch",$headers,$post_server);
+	$poster = new poster();
+	$r = $poster->post($post, "/download/batch");
 	if(in_array("Maintenance: 1", $r[0])){
 		return [];
 	}
@@ -141,49 +70,13 @@ function download_event($post) {
 	global $uid, $mysql, $additional_for_android, $additional_for_ios;
 	include 'config/modules_download.php';
 	include 'config/maintenance.php';
-	$tokenanduid = login();
-	if($tokenanduid == "Maintenance"){
-		return [];
-	}
-	$token = $tokenanduid['authorize_token'];
-	$user_id = $tokenanduid['user_id'];
-	$sessionKey = $tokenanduid['sessionKey'];
-	$os = $post['os'];
-	if($post['os'] == 'Android'){
-		$PlatformType = 2;
-	}else{
-		$PlatformType = 1;
-	}
-	
-	$time = time();
-	$headers = array(
-			'Accept: */*',
-			'Accept-Encoding: deflate',
-			'API-Model: straightforward',
-			'Debug: 1',
-			"Bundle-Version: $official_bundle_ver",
-			"Client-Version: $official_client_ver",
-			'OS-Version: Nexus 5 google hammerhead 4.4.4',
-			"OS: $os",
-			"Platform-Type: $PlatformType",
-			'Application-ID: 626776655',
-			'Time-Zone: JST',
-			'Reigion: 392',
-			"Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=3",
-			'Expect:'
-		);
 	
 	$post['timeStamp'] = time();
-	$post['commandNum'] = $login_key.".".time()."."."3";
+	$post['commandNum'] = $login_key.".".time()."."."4";
 	
-	$XMC = hash_hmac('sha1', json_encode($post), $sessionKey);
-	$headers[] = "X-Message-Code: $XMC";
-	$headers[] = "User-ID: $user_id";
+	$poster = new poster();
+	$r = $poster->post($post, "/download/event");
 	
-	$post_server = array(
-		"request_data" => json_encode($post)
-	);
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/download/event",$headers,$post_server);
 	if(in_array("Maintenance: 1", $r[0])){
 		return [];
 	}
@@ -210,55 +103,12 @@ function download_getUrl($post) {
 function download_update($post) {
 	global $uid, $mysql;
 	include 'config/modules_download.php';
-	pl_assert($post);
-	pl_assert(isset($post['install_version']), '请升级到4.0客户端！');
-	if ($post['os'] != 'Android' && $post['os'] != 'iOS') {
-	return [];
-	}
-	$os = $post['os'];
-	if($post['os'] == 'Android'){
-		$PlatformType = 2;
-	}else{
-		$PlatformType = 1;
-	}
-	$post['install_version'] = $official_client_ver;
-	include 'config/maintenance.php';
-	$tokenanduid = login();
-	$token = $tokenanduid['authorize_token'];
-	$user_id = $tokenanduid['user_id'];
-	$sessionKey = $tokenanduid['sessionKey'];
-	$user_cli_ver = $official_client_ver > $post['external_version']?$official_client_ver:$post['external_version'];
-	
-	$time = time();
-	$headers = array(
-			'Accept: */*',
-			'Accept-Encoding: deflate',
-			'API-Model: straightforward',
-			'Debug: 1',
-			"Bundle-Version: $official_bundle_ver",
-			"Client-Version: $user_cli_ver",
-			'OS-Version: Nexus 5 google hammerhead 4.4.4',
-			"OS: $os",
-			"Platform-Type: $PlatformType",
-			'Application-ID: 626776655',
-			'Time-Zone: JST',
-			'Reigion: 392',
-			"Authorize: consumerKey=lovelive_test&timeStamp=$time&version=1.1&token=$token&nonce=3",
-			'Expect:'
-		);
 	
 	$post['timeStamp'] = time();
 	$post['commandNum'] = $login_key.".".time()."."."3";
 	
-	require 'config/code.php';
-	$XMC = hash_hmac('sha1', json_encode($post), $sessionKey);
-	$headers[] = "X-Message-Code: $XMC";
-	$headers[] = "User-ID: $user_id";
-	
-	$post_server = array(
-		"request_data" => json_encode($post)
-	);
-	$r = curls("prod-jp.lovelive.ge.klabgames.net/main.php/download/update",$headers,$post_server);
+	$poster = new poster();
+	$r = $poster->post($post, "/download/update");
 	$ret = $r[1]['response_data'];
 	$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
 	if($download_site == "1"){
