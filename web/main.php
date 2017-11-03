762da8a85e55ba92ec943cb3aaee5a9a2fcab68b
lijun0326 <lijun00326@gmail.com>
2017-11-03 16:49:41 +0800

    新item（SR以上技能券）

--- web/main.php
+++ web/main.php
@@ -129,7 +129,8 @@ if (isset($uid)) {
 	$__user_bak = $user;
 	//如果没有某些常用值，置初值，免得代码里判断
 	$param_list=['enable_card_switch', 'card_switch', 'random_switch', 'extend_mods_key', 'allow_test_func',
-	 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13', 'item14', 'aqours_flag'];
+	 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13', 'item14', 'item15', 
+	 'aqours_flag'];
 	foreach ($param_list as $name) {
 		if (!isset($params[$name])) {
 			$params[$name] = 0;
