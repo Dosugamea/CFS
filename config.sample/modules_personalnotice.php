ab55183557f7a1c9877a2df489508c311b4eb8db
lijun0326 <lijun00326@gmail.com>
2017-11-09 16:13:36 +0800

    增加personalnotice模块

---
+++ config.sample/modules_personalnotice.php
@@ -0,0 +1,17 @@
+<?php
+//配置推送给全服所有用户的公告
+$global_notice = [];
+//请按如下格式配置
+//$global_notice[] = [
+//	"notice_id" => 1,
+//	"type"      => 2, （此处1不带勾选框，2带）
+//	"title"     => "标题",
+//	"contents"   => "内容"
+//]
+$global_notice[] = [
+	"notice_id" => 1,
+	"type"      => 2,
+	"title"     => "标题",
+	"contents"   => "内容"
+]
+?>
