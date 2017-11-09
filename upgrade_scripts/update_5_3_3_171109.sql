36407ca308eddbfb597078c99ea8547edee5496b
lijun0326 <lijun00326@gmail.com>
2017-11-09 16:15:55 +0800

    sql写漏个逗号

--- upgrade_scripts/update_5_3_3_171109.sql
+++ upgrade_scripts/update_5_3_3_171109.sql
@@ -6,7 +6,7 @@ CREATE TABLE `personalnotice` (
 	`content` TEXT NOT NULL,
 	`agree` TINYINT(4) NOT NULL DEFAULT '0',
 	`insert_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`agree_date` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00'
+	`agree_date` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
 	UNIQUE INDEX `Index 1` (`user_id`, `notice_id`)
 )
 COLLATE='utf8mb4_general_ci'
@@ -15,7 +15,7 @@ ENGINE=InnoDB
 CREATE TABLE `personalnotice_global` (
 	`user_id` INT(11) NOT NULL,
 	`notice_id` INT(11) NOT NULL,
-	`insert_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	`insert_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	UNIQUE INDEX `Index 1` (`user_id`, `notice_id`)
 )
 ENGINE=InnoDB
