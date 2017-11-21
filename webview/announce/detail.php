485d71c97d68bf0cc644b5a2d29e157d3676eaaf
NOAHLUALU <919400283@qq.com>
2017-11-21 09:40:04 +0800

    修复通知详细公告页

--- webview/announce/detail.php
+++ webview/announce/detail.php
@@ -1,66 +1,37 @@
-<?php
-$announcement=$mysql->query('select * from webview where id='.($_GET['announce_id']))->fetch();
-$time=explode(' ', $announcement['time'])[0];
+<?
+  $k = $mysql -> query("SELECT * FROM webview WHERE tab = 0 AND ID =".$_GET['detail_id']." LIMIT 1")->fetch();
 ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="GENERATOR" content="MSHTML 11.00.10011.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=880, target-densitydpi=device-dpi, user-scalable=no">
 
-<!DOCTYPE HTML>
-<!DOCTYPE html PUBLIC "" ""><HTML lang="ja"><HEAD><META content="IE=11.0000" 
-http-equiv="X-UA-Compatible">
- 
-<META charset="utf-8"> 
-<!--<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />-->
-<TITLE>news 
-detail</TITLE> 
-<LINK href="/resources/bstyle.css" rel="stylesheet"> 
-<LINK href="/resources/news.css" rel="stylesheet"> 
-<link href="/resources/css/style.css" rel="stylesheet">
-<STYLE>
-    html, body {
-    background-color: transparent;
-  }
-p{     
-    background-image: url(/resources/bug_trans.png); 
-}
-</STYLE>
- 
-<SCRIPT type="text/javascript">
- window.onload = function() {
- setTimeout(function(){window.scrollTo(0, 1);}, 100);
- }
+    <link rel="stylesheet" href="/resources/things/detail.css">
+    <link rel="stylesheet" href="/resources/things/perfect-scrollbar.css">
+    <link rel="stylesheet" href="/resources/things/list2.css">
 
-var strUA = "";
-strUA = navigator.userAgent.toLowerCase();
-
-if(strUA.indexOf("iphone") >= 0) {
-  document.write('<meta name="viewport" content="width=100%, minimum-scale=0.45, maximum-scale=0.45" />');
-} else if (strUA.indexOf("ipad") >= 0) {
-  document.write('<meta name="viewport" content="width=100%, minimum-scale=0.9, maximum-scale=0.9" />');
-} else {
-	  document.write('<meta name="viewport" content="width=100%, minimum-scale=0.38, maximum-scale=0.38" />');
-}
-</SCRIPT>
- 
-<META name="GENERATOR" content="MSHTML 11.00.10011.0"></HEAD> 
-<BODY>
-<DIV id="wrapper">
-<DIV class="title_news fs34"><SPAN class="ml30"><?=$announcement['title']?></SPAN>     
-<A id="back" 
-href="/webview.php/announce/announce?disp_faulty=<?=$_GET['disp_faulty']?>">
-<DIV class="topback"><IMG src="/resources/com_button_01.png" data-on="/resources/com_button_02se.png"> 
-        </DIV></A> </DIV>
-<DIV class="content_news">
-<DIV class="note">
-<P><?=$announcement['content']?><BR><BR></P><?=$time?>              <A 
-id="back" href="/webview.php/announce/announce?disp_faulty=<?=$_GET['disp_faulty']?>">
-<DIV class="bottomback"><IMG src="/resources/com_button_01.png" 
-data-on="/resources/com_button_02se.png"> 
-            </DIV></A>   </DIV></DIV>
-<DIV class="footer_news fs34"><IMG width="100%" src="/resources/bg03.png"> 
-</DIV></DIV>
-<SCRIPT>
-document.getElementById('back').addEventListener('touchstart', function(ev){
-  var img = this.querySelector('img');
-  img.src = img.getAttribute('data-on');
-}, false);
-</SCRIPT>
- </BODY></HTML>
+    <script src="/resources/things/perfect-scrollbar.min.js"></script>
+    <script src="/resources/things/button.js"></script>
+  </head>
+  <body>
+    <div id="outer">
+      <div id="inner">
+        <div id="header">
+          <?=$k['title']?>
+          <div id="back"></div>
+        </div>
+        <div id="body">
+          <?=$k['content']?>
+        </div>
+      </div>
+    </div>
+    <script>
+      Button.initialize(document.getElementById('back'), function() {
+        window.history.go(-1);
+      });
+      Ps.initialize(document.getElementById('body'), {suppressScrollX: true});
+    </script>
+  </body>
+</html>
