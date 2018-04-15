9258998507abc00d022742dc2827e2336f118dc4
lijun0326 <lijun00326@gmail.com>
2018-04-15 16:56:28 +0800

    修复install忘记推数据库结构

--- install.sql
+++ install.sql
@@ -300,6 +300,25 @@ CREATE TABLE IF NOT EXISTS `packages` (
   KEY `version` (`version`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6394 DEFAULT CHARSET=utf8;
 
+CREATE TABLE `personalnotice` (
+	`user_id` INT(11) NOT NULL,
+	`notice_id` INT(11) NOT NULL,
+	`type` INT(11) NOT NULL DEFAULT '1',
+	`title` TEXT NOT NULL,
+	`content` TEXT NOT NULL,
+	`agree` TINYINT(4) NOT NULL DEFAULT '0',
+	`insert_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`agree_date` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+	UNIQUE INDEX `Index 1` (`user_id`, `notice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `personalnotice_global` (
+	`user_id` INT(11) NOT NULL,
+	`notice_id` INT(11) NOT NULL,
+	`insert_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE INDEX `Index 1` (`user_id`, `notice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- 数据导出被取消选择。
 -- 导出  表 lovelive.programmed_live 结构
 CREATE TABLE IF NOT EXISTS `programmed_live` (
