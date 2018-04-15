926d59934b48b10ba0c360333a291753ea84647e
lijun0326 <lijun00326@gmail.com>
2018-04-15 16:59:24 +0800

    切回旧版下载模式

--- modules/download.php
+++ modules/download.php
@@ -5,91 +5,28 @@
 function download_additional($post) {
 	global $uid, $mysql, $additional_for_android, $additional_for_ios;
 	include '../config/modules_download.php';
-	/*
-	pl_assert(($_SERVER['HTTP_OS'] == 'Android' && $additional_for_android) || ($_SERVER['HTTP_OS'] == 'iOS' && $additional_for_ios), '暂不提供您区系统的客户端的数据包下载！');
 	$ret = $mysql->query(
 		"SELECT update_id as download_additional_id, url, size FROM packages
-		WHERE package_id=? AND package_type=? AND os=?"
+		WHERE package_id = ? AND package_type = ? AND os = ?"
 	, [$post['package_id'], $post['package_type'], $post['os']])->fetchAll(PDO::FETCH_ASSOC);
 	pl_assert($ret, "找不到下载：ID：{$post['package_id']} 种类：{$post['package_type']} 系统：{$post['os']}");
 	return $ret;
-	*/
-	include '../config/maintenance.php';
-	
-	$post['timeStamp'] = time();
-	$post['commandNum'] = $login_key.".".time()."."."3";
-	
-	$poster = new poster();
-	$r = $poster->post($post, "/download/additional");
-	$ret = $r[1]['response_data'];
-	$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
-	if($download_site == "1"){
-		$ret = json_encode($ret);
-		$ret = (str_replace('dnw5grz2619mn.cloudfront.net', $reverse_proxy,$ret));
-		$ret = json_decode($ret, true);
-	}
-	return removeQueryStrings($ret);
 }
 
 function download_batch($post) {
 	global $uid, $mysql, $additional_for_android, $additional_for_ios;
-	include '../config/modules_download.php';
-	//虹原的旧代码先注释掉啦=w=
-	/*pl_assert(($_SERVER['HTTP_OS'] == 'Android' && $additional_for_android) || ($_SERVER['HTTP_OS'] == 'iOS' && $additional_for_ios), '暂不提供您区系统的客户端的数据包下载！');
 	$ret = $mysql->query(
 		"SELECT package_id, url, size FROM packages
-		WHERE package_type=? AND os=?"
+		WHERE package_type = ? AND os = ?"
 	, [$post['package_type'], $post['os']])->fetchAll(PDO::FETCH_ASSOC);
 	$ret = array_merge(array_filter($ret, function ($e) use ($post) {
 		return array_search($e['package_id'], $post['excluded_package_ids']) === false;
 	}));
-	return $ret;*/
-	include '../config/maintenance.php';
-	
-	$post['timeStamp'] = time();
-	$post['commandNum'] = $login_key.".".time()."."."3";
-	
-	$poster = new poster();
-	$r = $poster->post($post, "/download/batch");
-	if(in_array("Maintenance: 1", $r[0])){
-		return [];
-	}
-	else{
-		$ret = $r[1]['response_data'];
-		$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
-		if($download_site == "1"){
-			$ret = json_encode($ret);
-			$ret = (str_replace('dnw5grz2619mn.cloudfront.net', $reverse_proxy,$ret));
-			$ret = json_decode($ret, true);
-		}
-		return removeQueryStrings($ret);
-	}
+	return $ret;
 }
 
 function download_event($post) {
-	global $uid, $mysql, $additional_for_android, $additional_for_ios;
-	include '../config/modules_download.php';
-	include '../config/maintenance.php';
-	
-	$post['timeStamp'] = time();
-	$post['commandNum'] = $login_key.".".time()."."."4";
-	
-	$poster = new poster();
-	$r = $poster->post($post, "/download/event");
-	
-	if(in_array("Maintenance: 1", $r[0])){
-		return [];
-	}
-	else{
-		$ret = $r[1]['response_data'];
-		$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
-		if($download_site == "1"){
-			$ret = json_encode($ret);
-			$ret = (str_replace('dnw5grz2619mn.cloudfront.net', $reverse_proxy,$ret));
-			$ret = json_decode($ret, true);
-		}
-		return removeQueryStrings($ret);
-	}
+	return download_batch($post);
 }
 
 function download_getUrl($post) {
@@ -102,52 +39,53 @@ function download_getUrl($post) {
 //download/update 下载更新
 function download_update($post) {
 	global $uid, $mysql;
-	include '../config/modules_download.php';
-	
-	$post['timeStamp'] = time();
-	$post['commandNum'] = $login_key.".".time()."."."3";
-	
-	$poster = new poster();
-	$r = $poster->post($post, "/download/update");
-	$ret = $r[1]['response_data'];
-	$download_site = $mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
-	if($download_site == "1"){
-		$ret = json_encode($ret);
-		$ret = (str_replace('dnw5grz2619mn.cloudfront.net', $reverse_proxy, $ret));
-		$ret = json_decode($ret, true);
+	if ($post['os'] != 'Android' && $post['os'] != 'iOS') {
+		return [];
 	}
-	$extend = $mysql->query('
-		SELECT extend_download.* FROM extend_download_queue
-		LEFT JOIN extend_download
-		ON extend_download.ID=extend_download_queue.download_id
-		WHERE downloaded_version < version OR downloaded_version=0
-		AND extend_download_queue.user_id='.$uid
-	)->fetchAll(PDO::FETCH_ASSOC);
-	foreach($extend as $i){
-		$i['size'] = (int)$i['size'];
-		$ret[] = $i;
+	$ver = version_compare($post['install_version'], $post['external_version'], '<') ? $post['install_version'] : $post['external_version'];
+	$ver = explode('.', $ver);
+	$ver = $ver[0] * 1000 + $ver[1];
+	$pkg = ['99_1'];
+	foreach ($post['package_list'] as $v) {
+		$pkg[] = $v['package_type'] . '_' . $v['package_id'];
 	}
-	$mysql->query("DELETE FROM extend_download_queue WHERE user_id = ".$uid);
-	return removeQueryStrings($ret);
-}
-
-function removeQueryStrings($ret){
-	global $uid, $mysql;
-	include '../config/modules_download.php';
-	
-	$download_site = (int)$mysql->query('SELECT download_site FROM users WHERE user_id='.$uid)->fetch()[0];
-	$package_path = $_SERVER['HTTP_OS'] == 'iOS' ? $check_package_ios : $check_package_an;
-	if($reverse_proxy && $download_site == 1){
-		foreach($ret as &$i){
-			$url = explode("?", $i['url'])[0];
-			$fhash = substr($url, -43, 32);
-			$fullfname = $package_path.$fhash;
-			if(is_file($fullfname)){
-				$i['url'] = $url;
+	$dl = $mysql->query("SELECT * FROM packages WHERE os='{$post['os']}' AND version > $ver AND concat(package_type, '_', package_id) in ('" . implode($pkg, "','") . "')");
+	$ret = [];
+	while ($col = $dl->fetch()) {
+		$ret[] = [
+			"download_update_id" => (int)$col['update_id'],
+			"url" => $col['url'],
+			"size" => (int)$col['size'],
+			"version" => floor($col['version'] / 1000) . '.' . ($col['version'] % 1000),
+			"package_type" => (int)$col['package_type'],
+			"package_id" => (int)$col['package_id']
+		];
+	}
+	$version = [];
+	$order = [];
+	foreach ($ret as $v) {
+		$order[] = array_search($v['package_type'] . '_' . $v['package_id'], $pkg);
+		$version[] = $v['version'];
+	}
+	array_multisort($version, $order, $ret);
+	//下载私服专用包
+	$res = $mysql->query(
+	'SELECT extend_download.* FROM extend_download LEFT JOIN extend_download_queue
+	ON extend_download.ID = extend_download_queue.download_id
+	WHERE user_id='.$uid.' AND (downloaded_version < version OR downloaded_version=0)')->fetchAll();
+	if (!empty($res)) {
+		foreach ($res as $row) {
+			$ret2['download_update_id'] = (int)$row[0] * -1;
+			$ret2['url'] = $row[1];
+			$ret2['size'] = (int)$row[2];
+			$ret[] = $ret2;
+			if ($row['version'] == 0) {
+			$mysql->exec("DELETE FROM extend_download_queue WHERE user_id=$uid AND download_id={$row[0]}");
+			} else {
+			$mysql->exec("UPDATE extend_download_queue SET downloaded_version={$row['version']} WHERE user_id='.$uid AND download_id={$row[0]}");
 			}
 		}
 	}
-	
 	return $ret;
 }
 ?>
