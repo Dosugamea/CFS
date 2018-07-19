addda3a5ba2839d0593d3228465658b3952dcb5b
lijun0326 <lijun00326@gmail.com>
2018-07-19 23:53:48 +0800

    RSA模块适配

--- includes/RSA.php
+++ includes/RSA.php
@@ -1,21 +1,14 @@
 <?php
 function RSAdecrypt($encrypted){
-	include('../config/RSA_KeyPair.php');
-	$priv_key =  openssl_pkey_get_private($priv_key);
-	openssl_private_decrypt(base64_decode($encrypted),$decrypted,$priv_key);
+	global $config;
+	$priv_key =  openssl_pkey_get_private($config->basic['priv_key']);
+	openssl_private_decrypt(base64_decode($encrypted), $decrypted, $priv_key);
 	return $decrypted;
 }
 
-function RSAencrypt($data){
-	include('../config/RSA_KeyPair.php');
-	$official_pub_key =  openssl_pkey_get_public($official_pub_key);
-	openssl_public_encrypt($data, $encrypted, $official_pub_key);
-	return base64_encode($encrypted);
-}
-
 function RSAsign($data){
-	include('../config/RSA_KeyPair.php');
-	$priv_key =  openssl_pkey_get_private($priv_key);
+	global $config;
+	$priv_key =  openssl_pkey_get_private($config->basic['priv_key']);
 	openssl_sign($data, $signature, $priv_key, OPENSSL_ALGO_SHA1);
 	return base64_encode($signature);
 }
