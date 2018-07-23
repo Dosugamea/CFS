b4764bb7fd8358c11bc4997cc621bde649f00c56
lijun0326 <lijun00326@gmail.com>
2018-07-24 01:26:04 +0800

    背景模块适配

--- modules/background.php
+++ modules/background.php
@@ -1,21 +1,21 @@
 <?php
 function background_backgroundInfo() {
-  global $user;
-  if ($user['background'] == 0) {
-    $user['background'] = 1;
+  global $envi;
+  if ($envi->user['background'] == 0) {
+    $envi->user['background'] = 1;
   }
   $ret['background_info'] = [];
   for($i = 1; $i <= 24; $i++) {
     $ret['background_info'][]=[
       'background_id'=>$i,
-      'is_set'=>$i==$user['background'],
+      'is_set'=>$i==$envi->user['background'],
       "insert_date"=>"2013-04-15 00:00:00"
     ];
   }
   for($i = 901; $i <= 902; $i++) {
     $ret['background_info'][]=[
       'background_id'=>$i,
-      'is_set'=>$i==$user['background'],
+      'is_set'=>$i==$envi->user['background'],
       "insert_date"=>"2013-04-15 00:00:00"
     ];
   }
@@ -23,7 +23,7 @@ function background_backgroundInfo() {
 }
 
 function background_set($post) {
-  global $user;
-  $user['background'] = $post['background_id'];
+  global $envi;
+  $envi->user['background'] = $post['background_id']; //TODO
   return [];
 }
