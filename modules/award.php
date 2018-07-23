ea1ba4bd7df258ca5e3f023d1c0a2c7942b60e20
lijun0326 <lijun00326@gmail.com>
2018-07-24 01:25:29 +0800

    头衔模块适配

--- modules/award.php
+++ modules/award.php
@@ -1,23 +1,22 @@
 <?php
 function award_awardInfo() {
-	include_once("../config/modules_award.php");
-	global $user, $mysql, $uid;
-	if ($user['award'] == 0) {
-		$user['award'] = 1;
+	global $envi, $mysql, $uid, $config;
+	if ($envi->user['award'] == 0) {
+		$envi->user['award'] = 1;
 	}
 	$ret['award_info'] = [];
-	foreach($default_awards as $i){
+	foreach($config->m_awards['default_awards'] as $i){
 		$ret['award_info'][]=[
 		'award_id'=>$i,
-		'is_set'=>$i==$user['award'],
+		'is_set'=>$i==$envi->user['award'],
 		"insert_date"=>"2013-04-15 00:00:00"
 		];
 	}
-	$extra_awards = $mysql->query("SELECT * FROM award WHERE user_id = ?", [$uid])->fetchAll(PDO::FETCH_ASSOC);
+	$extra_awards = $mysql->query("SELECT * FROM award WHERE user_id = ?", [$uid])->fetchAll();
 	foreach($extra_awards as $i){
 		$ret['award_info'][]=[
 		'award_id'=>(int)$i['award_id'],
-		'is_set'=>(int)$i['award_id']==$user['award'],
+		'is_set'=>(int)$i['award_id']==$envi->user['award'],
 		"insert_date"=>$i['insert_date']
 		];
 	}
@@ -25,18 +24,17 @@ function award_awardInfo() {
 }
 
 function award_set($post) {
-	include_once("../config/modules_award.php");
-	global $user, $mysql, $uid;
+	global $envi, $mysql, $uid, $config;
 	if(!is_numeric($post['award_id']))
 		throw403("INVALID_DATA");
-	if(!in_array($post['award_id'], $default_awards)){
-		$extra_awards = $mysql->query("SELECT * FROM award WHERE user_id = ?", [$uid])->fetchAll(PDO::FETCH_ASSOC);
+	if(!in_array($post['award_id'], $config->m_awards['default_awards'])){
+		$extra_awards = $mysql->query("SELECT * FROM award WHERE user_id = ?", [$uid])->fetchAll();
 		foreach($extra_awards as $i)
 			if((int)$i['award_id'] == $post['award_id'])
 				$result = true;
 		if(!$result)
 			throw403("AWARD_NOT_EXIST");
 	}
-	$user['award'] = $post['award_id'];
+	$envi->user['award'] = $post['award_id'];
 	return [];
 }
