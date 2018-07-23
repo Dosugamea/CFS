68f44a7fa89a0b037730814c5f7a611f42ba9dee
lijun0326 <lijun00326@gmail.com>
2018-07-24 01:28:43 +0800

    programmed适配

--- modules/programmed.php
+++ modules/programmed.php
@@ -1,11 +1,10 @@
 <?php
 //programmed.php 私服客户端专用功能
 function programmed_getCustomLiveList($post = []) {
-	global $params;
 	if (!isset($post['api_ver'])) {
 		$post['api_ver'] = 0;
 	}
-	global $mysql, $uid, $params;
+	global $mysql, $uid, $envi;
 	$list = array_map(function ($e) use ($post) {
 		$live = json_decode($e['live_json'], true);
 		return [
@@ -16,8 +15,8 @@ function programmed_getCustomLiveList($post = []) {
 			'status' => 1,
 			'category' => $e['category'] ? $e['category'] : 'Custom Live',
 		];
-	}, $mysql->query('select * from programmed_live')->fetchAll());
-	if ($params['allow_test_func'] == 0) {
+	}, $mysql->query('SELECT * from programmed_live')->fetchAll());
+	if ($envi->params['allow_test_func'] == 0) {
 		$list = array_filter($list, function ($v) {
 			global $uid;
 			if (isset($v['live']['developer_only']) && !isset($v['live']['upload_user'])) {
@@ -43,7 +42,7 @@ function programmed_getCustomLiveList($post = []) {
 	}, $list);
 	$played = $mysql->query('
 		SELECT distinct notes_setting_asset FROM live_ranking
-		WHERE user_id='.$uid.' AND card_switch='.$params['card_switch'].' AND random_switch='.$params['random_switch'].'
+		WHERE user_id='.$uid.' AND card_switch='.$envi->params['card_switch'].' AND random_switch='.$envi->params['random_switch'].'
 		AND notes_setting_asset in ("'.implode('","', $notes_setting_asset_list).'")
 	')->fetchAll(PDO::FETCH_COLUMN);
 	foreach ($list as &$v) {
