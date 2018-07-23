26711864a1bee25b7b36d1b283636ac9b84fcdbe
lijun0326 <lijun00326@gmail.com>
2018-07-24 01:26:19 +0800

    banner模块重写

--- modules/banner.php
+++ modules/banner.php
@@ -3,63 +3,37 @@
 
 //banner/bannerList 获取banner列表 不返回的话不影响使用，但教程会卡在第8步
 function banner_bannerList() {
-	global $params;
-	$ret = json_decode('{
-		"time_limit": "'.Date('Y-m-d').' 23:59:59",
-		"member_category_list": [{
-			"member_category": 1,
-			"banner_list": []
-		}, {
-			"member_category": 2,
-			"banner_list": []
-		}]
-	}', true);
-	include("../config/event.php");
-	if(strtotime($marathon['start_date']) < time() && strtotime($marathon['end_date']) > time()){
-		$ret['member_category_list'][0]['banner_list'][] = [
-			"banner_type"            => 0,
-			"target_id"              => $marathon['event_id'],
-			"asset_path"             => $marathon['asset_path'],
-			"asset_path_se"          => $marathon['asset_path_se'],
-			"master_is_active_event" => true
-		];
-	}
-	if(strtotime($battle['start_date']) < time() && strtotime($battle['end_date']) > time()){
-		$ret['member_category_list'][0]['banner_list'][] = [
-			"banner_type"            => 0,
-			"target_id"              => $battle['event_id'],
-			"asset_path"             => $battle['asset_path'],
-			"asset_path_se"          => $battle['asset_path_se'],
-			"master_is_active_event" => true
-		];
-	}
-	if(strtotime($festival['start_date']) < time() && strtotime($festival['end_date']) > time()){
-		$ret['member_category_list'][0]['banner_list'][] = [
-			"banner_type"            => 0,
-			"target_id"              => $festival['event_id'],
-			"asset_path"             => $festival['asset_path'],
-			"asset_path_se"          => $festival['asset_path_se'],
-			"master_is_active_event" => true
-		];
-	}
-	if(strtotime($challenge['start_date']) < time() && strtotime($challenge['end_date']) > time()){
-		$ret['member_category_list'][0]['banner_list'][] = [
-			"banner_type"            => 0,
-			"target_id"              => $challenge['event_id'],
-			"asset_path"             => $challenge['asset_path'],
-			"asset_path_se"          => $challenge['asset_path_se'],
-			"master_is_active_event" => true
-		];
-	}
-	if($params['card_switch']==1 && 
-		strtotime($duty['start_date']) < time() && strtotime($duty['end_date']) > time()){
-		$ret['member_category_list'][0]['banner_list'][] = [
-			"banner_type"            => 0,
-			"target_id"              => $duty['event_id'],
-			"asset_path"             => $duty['asset_path'],
-			"asset_path_se"          => $duty['asset_path_se'],
-			"master_is_active_event" => true
-		];
+	global $envi, $config;
+	$ret = [
+		"time_limit" => Date('Y-m-d')." 23:59:59",
+		"member_category_list" => [[
+			"member_category" => 1,
+			"banner_list" => []
+		], [
+			"member_category"=> 2,
+			"banner_list" => []
+		]]
+	];
+
+	$genEventDetail = function ($event) use (&$ret){
+		if(strtotime($event['start_date']) < time() && strtotime($event['end_date']) > time()){
+			$ret['member_category_list'][0]['banner_list'][] = [
+				"banner_type"            => 0,
+				"target_id"              => $event['event_id'],
+				"asset_path"             => $event['asset_path'],
+				"asset_path_se"          => $event['asset_path_se'],
+				"master_is_active_event" => true
+			];
+		}
+	};
+
+	$genEventDetail($config->event['marathon']);
+	$genEventDetail($config->event['battle']);
+	$genEventDetail($config->event['festival']);
+	$genEventDetail($config->event['challenge']);
+	
+	if($envi->params['card_switch'] == 1){
+		$genEventDetail($config->event['duty']);
 	}
 	
 	$ret['member_category_list'][1]['banner_list'] = $ret['member_category_list'][0]['banner_list'];
