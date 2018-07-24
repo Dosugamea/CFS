857f96a1d5aa88d33d5915afea1d900abcfbd08c
lijun0326 <lijun00326@gmail.com>
2018-07-24 20:03:43 +0800

    删除不必要的config.sample

--- config.sample/RSA_KeyPair.php
+++
@@ -1,5 +0,0 @@
-<?php
-//在这配置PL用的密钥对
-$pub_key = '';
-$priv_key = '';
-$official_pub_key = ''; //官方公钥
--- config.sample/modules_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-//配置XOR base key
-$base_key = "eit4Ahph4aiX4ohmephuobei6SooX9xo";
-//application key
-$application_key = "gae1aefu9eeRojahj4cei3pho1eaZada";
-?>
