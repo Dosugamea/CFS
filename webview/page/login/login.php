09876cb6bd1f0053e033ca758bd9b1ff348bc908
lijun0326 <lijun00326@gmail.com>
2018-08-06 01:44:02 +0800

    修复webview的login/login

--- webview/page/login/login.php
+++ webview/page/login/login.php
@@ -1,21 +1,13 @@
-<?
- 	$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
- 	if(strpos($agent, 'iphone') || strpos($agent, 'ipad') )
- 		$device_type = 'ios';
- 	else
- 		$device_type = 'other';
- 	
-?>
 <header class="mdui-appbar mdui-appbar-fixed">
 	<div class="mdui-toolbar mdui-color-theme">
 		<span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" >
 			<i class="mdui-icon material-icons" onclick="location.href='/webview.php/login/welcome'">arrow_back</i>
 		</span>
-		<a class="mdui-typo-title" style="text-transform:capitalize;"><?=$action?></a>
+		<a class="mdui-typo-title" style="text-transform:capitalize;">登录</a>
 		<div class="mdui-toolbar-spacer"></div>
 	</div>
 </header>
-<div class="mdui-container" <?if($device_type == 'ios') print('style="display:none;"'); ?>>
+<div class="mdui-container" <?if($result['device_type'] == 'ios') print('style="display:none;"'); ?>>
 	<div class="doc-container">
 		<form method="post" action="/webview.php/login/login" autocomplete="off">
 			<div class="mdui-textfield mdui-textfield-floating-label">
@@ -34,9 +26,9 @@
 	</div>
 </div>
 
-<div class="mdui-container framecard" <?if($device_type == 'other') print('style="display:none;"'); ?>>
+<div class="mdui-container framecard" <?if($result['device_type'] == 'other') print('style="display:none;"'); ?>>
 	<div class="br"></div>
-	<div class="mdui-card" onclick="location.href='native://browser?url=http%3A%2F%2F<?=$_SERVER['SERVER_NAME']?>%2Fwebview%2Flogin%2Flogin_ios.php%3Ftoken%3D<?=$token?>%26username%3D<?=$username['username']?>'" >
+	<div class="mdui-card" onclick="location.href='native://browser?url=http%3A%2F%2F<?=$_SERVER['SERVER_NAME']?>%2Fwebview%2Flogin%2Flogin_ios.php%3Ftoken%3D<?=$token?>%26username%3D<?=$result['username']?>'" >
 	  	<div class="mdui-card-media">
 	    	<img src="/assets/img/apple_out.jpg"/>
 	    	<div class="mdui-card-media-covered">
