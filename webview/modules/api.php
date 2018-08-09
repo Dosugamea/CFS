fdcc8081b0aed9c8c92ca0387d098c2533d3703f
lijun0326 <lijun00326@gmail.com>
2018-08-10 01:25:23 +0800

    api增加reg

--- webview/modules/api.php
+++ webview/modules/api.php
@@ -49,3 +49,115 @@ function login_doLogin($post){
         return $result;
     }
 }
+
+function login_reg($post){
+    global $mysql, $authorize, $config;
+    require_once(BASE_PATH."includes/present.php");
+    require_once(BASE_PATH."includes/unit.php");
+    $unit = getUnitDb();
+
+    if(!isset($post['userId']) || !isset($post['nickname']) || !isset($post['inviter']) || !isset($post['password'])){
+        $result = [
+            "status" => -4,
+            "errmsg" => "参数错误"
+        ];
+        return $result;
+    }
+
+    //解密密码
+    @$post['password'] = RSAdecrypt($post['password']);
+    if($post['password'] === NULL){
+        $result = [
+            "status" => 1,
+            "errmsg" => "密码解密失败"
+        ];
+        return $result;
+    }
+    
+    if (!is_numeric($post['userId'])) {
+        $result = [
+            "status" => 2,
+            "errmsg" => "UID异常"
+        ];
+        return $result;
+    }elseif($post['userId'] > 999999999){
+        $result = [
+            "status" => 3,
+            "errmsg" => "UID过大"
+        ];
+        return $result;
+    }else{
+        $check_uid = $mysql->prepare('SELECT user_id FROM users WHERE user_id = ?', [$post['userId']]);
+        if ($check_uid->rowCount()) {
+            $result = [
+                "status" => 4,
+                "errmsg" => "此ID已被注册，请更换"
+            ];
+            return $result;
+        }else{
+            $password = genpassv2($post['password'], $post['userId']);    
+            $mysql->query('
+                INSERT INTO `users` (`user_id`, `username`, `password`,`login_password`, `name`, `introduction`)
+                VALUES (?, ?, ?, ?, ?, "", ?)
+            ', [$post['userId'], $tmp_authorize['username'], $tmp_authorize['password'], $password, $post['name'], $post['site']]);
+            $param = $mysql->prepare("INSERT INTO user_params VALUES(".$post['userId'].", ?, ?)");
+            $param->execute(['enable_card_switch', $config->reg['disable_card_by_default'] ? 0 : 1]);
+            $param->execute(['card_switch', $config->reg['disable_card_by_default'] ? 0 : 1]);
+            $param->execute(['random_switch', 0]);
+            $param->execute(['allow_test_func', 0]);
+            $param->execute(['item1', 0]);
+            $param->execute(['item2', 0]);
+            $param->execute(['item3', 2525200]);
+            $param->execute(['item4', 0]);
+            $param->execute(['item5', 0]);
+    
+            //送三个初期宝石
+            $mysql->query("INSERT INTO removable_skill (user_id, skill_id, amount, equipped) VALUES(".$post['id'].",1,1,0)");
+            $mysql->query("INSERT INTO removable_skill (user_id, skill_id, amount, equipped) VALUES(".$post['id'].",2,1,0)");
+            $mysql->query("INSERT INTO removable_skill (user_id, skill_id, amount, equipped) VALUES(".$post['id'].",3,1,0)");
+        
+            $uid = $post['id'];
+            if($config->reg['all_card_by_default']) {
+                $card_list=$unit->query('SELECT unit_id from unit_m where unit_id <= ? and unit_number > 0', [$config->basic['max_unit_id']])->fetchAll();
+                foreach($card_list as $v){
+                    addUnit($v[0]);
+                }
+                //防止劝退
+                $mysql->query("UPDATE unit_list SET favorite_flag = 1 WHERE user_id = ?", [$post['id']]);
+            }
+        
+            $position = 1;
+            foreach($config->reg['default_deck_web'] as $k=>$v) {
+                $tmp['position'] = $position;
+                $tmp['unit_owning_user_id'] = addUnit($v)[0]['unit_owning_user_id'];
+                if($position == 5)
+                    $center = $tmp['unit_owning_user_id'];
+                $unit_deck_detail[] = $tmp;
+                $position++;
+            }
+        
+            $tmp2['unit_deck_detail'] = $unit_deck_detail;
+            $tmp2['unit_deck_id'] = 1;
+            $tmp2['main_flag'] = true;
+            $tmp2['deck_name'] = '';
+            $unit_deck_list[] = $tmp2;
+            $json = json_encode($unit_deck_list);
+            $mysql->query("INSERT INTO user_deck (`user_id`, `json`, `center_unit`) VALUES (?,?,?)", [$post['id'], $json, $center]);
+        
+            $mysql->query('delete from tmp_authorize where token = ?', [$token]);
+            $invite = (int)$post['invite'];
+            if($invite > 0){
+                $res = $mysql->query("SELECT user_id FROM users WHERE user_id = ?",[$invite])->fetch();
+                if($res){
+                    $mysql->query("INSERT INTO invitation (user_id, from_user) VALUES(?,?)", [$uid, $invite]);
+                    add_present("loveca", 10, "安利新人奖励", $uid = $invite);
+                }
+            }
+            $result = [
+                "status" => 0,
+                "errmsg" => ""
+            ];
+            return $result;
+        }
+    }
+}
