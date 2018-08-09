97c0f4702afe5e3fa8a8c88972a4aacf0847bcb0
NOAHLUALU <919400283@qq.com>
2018-08-09 16:11:53 +0800

    再次修复下载错误

--- modules/download.php
+++ modules/download.php
@@ -3,7 +3,7 @@
 
 //download/additional 下载附加内容
 function download_additional($post) {
-	global $uid, $mysql, $additional_for_android, $additional_for_ios;
+	global $uid, $mysql;
 	$ret = $mysql->query(
 		"SELECT update_id as download_additional_id, url, size FROM packages
 		WHERE package_id = ? AND package_type = ? AND os = ?"
