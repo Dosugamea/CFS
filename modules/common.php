5a6834777d3e53a64269bd4489af8c7286dad014
lijun0326 <lijun00326@gmail.com>
2018-10-09 18:17:08 +0800

    修复回复体力时心数显示0

--- modules/common.php
+++ modules/common.php
@@ -1,15 +1,17 @@
 <?php
 //common/recoveryEnergy 恢复LP防止崩溃
 function common_recoveryEnergy() {
-	global $params;
-	$before_sns_coin = $params['item4'];
+	global $envi;
+	$before_sns_coin = $envi->params['item4'];
 	if(!energyRecover()){
 		$ret = retError(1102);
 		return $ret;
 	}
 	$ret = getCurrentEnergy();
-	$ret['before_sns_coin'] = $before_sns_coin;
-	$ret['after_sns_coin'] = $params['item4'];
+	$ret['before_sns_coin'] 	= $before_sns_coin;
+	$ret['after_sns_coin']		= $envi->params['item4'];
+	$ret['item_list']			= runAction("user", "showAllItem")['items'];
+	$ret['server_timestamp']	= time();
 	return $ret;
 }
 ?>
