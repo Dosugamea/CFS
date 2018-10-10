8ee3343b36ac09c5772643657ae68352d06f1f20
lijun0326 <lijun00326@gmail.com>
2018-10-10 23:01:55 +0800

    energy模块修复各种诸如注入点

--- includes/energy.php
+++ includes/energy.php
@@ -1,22 +1,27 @@
 <?php
 //energy.php，负责LP管理、回复
+function getMaxEnergy(){
+	global $envi;
+	return 100 + floor($envi->user['level'] / 2);
+}
+
 function getCurrentEnergy($level=0){
 	global $uid, $mysql;
-	$energy = $mysql->query("SELECT energy_full_time, over_max_energy, level FROM users WHERE user_id = ".$uid)->fetch(PDO::FETCH_ASSOC);
-	if($level>0)
-		$energy['level']=$level;
-	$energy_max = 100 + floor($energy['level']/2);
+	$energy = $mysql->query("SELECT energy_full_time, over_max_energy, level FROM users WHERE user_id = ?", [$uid])->fetch(PDO::FETCH_ASSOC);
+	if($level > 0)
+		$energy['level'] = $level;
+	$energy_max = getMaxEnergy();
 	$ret = [];
 	$ret['energy_max'] = $energy_max;
 	if($energy['over_max_energy'] > $energy_max){
-		$ret['energy_full_need_time'] = 0;
-		$ret['energy_full_time'] = $energy['energy_full_time'];
-		$ret['over_max_energy'] = (int)$energy['over_max_energy'];
+		$ret['energy_full_need_time']	= 0;
+		$ret['energy_full_time']		= $energy['energy_full_time'];
+		$ret['over_max_energy']			= (int)$energy['over_max_energy'];
 		$ret['current_energy']			= $ret['over_max_energy'];
 	}else{
-		$ret['energy_full_need_time'] = (strtotime($energy['energy_full_time']) - strtotime("now")) < 0 ? 0 : (strtotime($energy['energy_full_time']) - strtotime("now"));
-		$ret['energy_full_time'] = $energy['energy_full_time'];
-		$ret['over_max_energy'] = 0;
+		$ret['energy_full_need_time']	= (strtotime($energy['energy_full_time']) - strtotime("now")) < 0 ? 0 : (strtotime($energy['energy_full_time']) - strtotime("now"));
+		$ret['energy_full_time']		= $energy['energy_full_time'];
+		$ret['over_max_energy']			= 0;
 		$ret['current_energy']			= $ret['energy_full_need_time'] == 0 ? $energy_max : floor((strtotime($energy['energy_full_time']) - time()) / 360);
 	}
 	return $ret;
@@ -24,30 +29,31 @@ function getCurrentEnergy($level=0){
 
 function energyDecrease($amount){
 	global $uid, $mysql;
-	$energy = $mysql->query("SELECT energy_full_time, over_max_energy, level FROM users WHERE user_id = ".$uid)->fetch(PDO::FETCH_ASSOC);
-	$energy_max = 100 + floor($energy['level']/2);
+	$energy = $mysql->query("SELECT energy_full_time, over_max_energy, level FROM users WHERE user_id = ?", [$uid])->fetch(PDO::FETCH_ASSOC);
+	$energy_max = getMaxEnergy();
 	if((strtotime($energy['energy_full_time']) - strtotime("now")) < 0)
 		$energy_now = $energy_max;
 	else
 		$energy_now = $energy_max - floor((strtotime($energy['energy_full_time']) - strtotime("now"))/360);
 	if($energy['over_max_energy'] > $energy_max){
 		if(($energy['over_max_energy'] - $amount) > $energy_max){
-			$mysql->query("UPDATE users SET over_max_energy = ".($energy['over_max_energy'] - $amount)." WHERE user_id = ".$uid);
+			$mysql->query("UPDATE users SET over_max_energy = ? WHERE user_id = ?", [$energy['over_max_energy'] - $amount, $uid]);
 			return true;
 		}else{
 			$decrease_cnt = $energy_max - ($energy['over_max_energy'] - $amount);
 			$energy_full_time = date("Y-m-d H:i:s",strtotime($energy['energy_full_time']) + $decrease_cnt * 360);
-			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = '".$energy_full_time."' WHERE user_id = ".$uid);
+			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = ? WHERE user_id = ?", [$energy_full_time, $uid]);
 			return true;
 		}
 	}else if(($energy_now - $amount) >= 0){
 		if($energy_now == $energy_max)
-			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = '". date("Y-m-d H:i:s",(time() + $amount * 360))."' WHERE user_id = ".$uid);
+			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = ? WHERE user_id = ?", [date("Y-m-d H:i:s",(time() + $amount * 360)), $uid]);
 		else
-			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = '". date("Y-m-d H:i:s",(strtotime($energy['energy_full_time']) + $amount * 360))."' WHERE user_id = ".$uid);
+			$mysql->query("UPDATE users SET over_max_energy = 0, energy_full_time = ? WHERE user_id = ?", [date("Y-m-d H:i:s",(strtotime($energy['energy_full_time']) + $amount * 360)), $uid]);
 		return true;
-	}else
+	}else{
 		return false;
+	}
 }
 
 function energyRecover($level = 0){
@@ -56,7 +62,7 @@ function energyRecover($level = 0){
 	if($level > 0){
 		$energy['level'] = $level;
 	}
-	$energy_max = 100 + floor($energy['level'] / 2);
+	$energy_max = getMaxEnergy();
 	if($energy['over_max_energy'] != 0 || strtotime($energy['energy_full_time']) <= time())
 		if($energy['over_max_energy'] >= $energy_max)
 			if($level==0)
