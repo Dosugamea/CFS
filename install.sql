3d04c34f056fc4ab2f358990787ae41f44fbca5c
lijun0326 <lijun00326@gmail.com>
2018-10-12 17:27:16 +0800

    修改数据库脚本

--- install.sql
+++ install.sql
@@ -492,6 +492,8 @@ CREATE TABLE IF NOT EXISTS `tmp_challenge_reward` (
 CREATE TABLE IF NOT EXISTS `tmp_duel_room` (
   `room_id` INT(11) NOT NULL AUTO_INCREMENT,
   `users` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
+  `difficulty` TINYINT(4) NOT NULL DEFAULT '0',
+  `card_switch` TINYINT(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- upgrade_scripts/update_6_3_2_181011.sql
+++ upgrade_scripts/update_6_3_2_181011.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `tmp_duel_room` (
 	`room_id` INT(11) NOT NULL AUTO_INCREMENT,
 	`users` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
+    `difficulty` TINYINT(4) NOT NULL DEFAULT '0',
+    `card_switch` TINYINT(4) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`room_id`)
 )
 COLLATE='utf8_general_ci'
